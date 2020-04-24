ee20282cd6f30a24f5da5bf006765c6eba3e8c2a
Janssen Garcia <janssen.garcia@gmail.com>
2020-04-24 14:52:06 -0300

    egundo commit

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -5,12 +5,16 @@ import java.util.concurrent.TimeUnit;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TasksTest {
 
 	public WebDriver acessarAplicacao(){
+
+
+		System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("http://localhost:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
